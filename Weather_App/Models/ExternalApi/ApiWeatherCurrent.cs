3bb0fe29c71bb6eaf19aba17a6ee30278a20c4da
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-06 11:21:00 +0200

    ExternalApi: Added ApiWeatherCurrent model.

---
+++ Weather_App/Models/ExternalApi/ApiWeatherCurrent.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+
+namespace Weather_App.Models.ExternalApi
+{
+    public class ApiWeatherCurrent
+    {
+        [JsonProperty("last_updated_epoch")]
+        public long? LastUpdatedEpoch { get; set; }
+        [JsonProperty("last_updated")]
+        public DateTime? LastUpdated { get; set; }
+        [JsonProperty("temp_c")]
+        public float? TemperatureCelsius { get; set; }
+        [JsonProperty("temp_f")]
+        public float? TemperatureFahrenheit { get; set; }
+        [JsonProperty("is_day")]
+        public bool IsDay { get; set; }
+        [JsonProperty("condition")]
+        public ApiWeatherCondition? Condition { get; set; }
+        [JsonProperty("wind_mph")]
+        public float? WindMPH { get; set; }
+        [JsonProperty("wind_kph")]
+        public float? WindKPH { get; set; }
+        [JsonProperty("wind_degree")]
+        public float? WindDegree { get; set; }
+        [JsonProperty("wind_dir")]
+        public string? WindDirection { get; set; }
+        [JsonProperty("pressure_mb")]
+        public float? PressureMilibars { get; set; }
+        [JsonProperty("pressure_in")]
+        public float? PressureHgInches { get; set; }
+        [JsonProperty("precip_mm")]
+        public float? PrecipMillimetres { get; set; }
+        [JsonProperty("precip_in")]
+        public float? PrecipInches { get; set; }
+        [JsonProperty("humidity")]
+        public float? Humidity { get; set; }
+        [JsonProperty("cloud")]
+        public float? Cloud { get; set; }
+        [JsonProperty("feelslike_c")]
+        public float? FeelsLikeTemperatureCelsius { get; set; }
+        [JsonProperty("feelslike_f")]
+        public float? FeelsLikeTemperatureFahrenheit { get; set; }
+        [JsonProperty("windchill_c")]
+        public float? WindChillCelsius { get; set; }
+        [JsonProperty("windchill_f")]
+        public float? WindChillFahrenheit { get; set; }
+        [JsonProperty("heatindex_c")]
+        public float? HeatIndexCelsius { get; set; }
+        [JsonProperty("heatindex_f")]
+        public float? HeatIndexFahrenheit { get; set; }
+        [JsonProperty("dewpoint_c")]
+        public float? DewpointCelsius { get; set; }
+        [JsonProperty("dewpoint_f")]
+        public float? DewpointFahrenheit { get; set; }
+        [JsonProperty("vis_km")]
+        public float? VisibilityKilometers { get; set; }
+        [JsonProperty("vis_miles")]
+        public float? VisibilityMiles { get; set; }
+        [JsonProperty("uv")]
+        public float? UVIndex { get; set; }
+        [JsonProperty("gust_mph")]
+        public float? GustMPH { get; set; }
+        [JsonProperty("gust_kph")]
+        public float? GustKPH { get; set; }
+    }
+}
---
+++ Weather_App/Models/ExternalApi/WeatherCurrent/ApiWeatherCurrent.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Weather_App.Models.ExternalApi.WeatherCurrent
+{
+    public class ApiWeatherForecast
+    {
+        [JsonProperty("location")]
+        public ApiWeatherLocation? Location { get; set; }
+        [JsonProperty("current")]
+        public ApiWeatherCurrent? Current { get; set; }
+    }
+}
