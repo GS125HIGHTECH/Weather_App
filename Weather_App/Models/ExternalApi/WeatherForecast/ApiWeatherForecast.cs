09373723dba46a95ff5db055d923c85b4aa1f287
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-06 11:21:37 +0200

    ExternalApi: Added ApiWeatherForecast model.

---
+++ Weather_App/Models/ExternalApi/WeatherForecast/ApiWeatherForecast.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Weather_App.Models.ExternalApi.WeatherForecast
+{
+    public class ApiWeatherForecast
+    {
+        [JsonProperty("location")]
+        public ApiWeatherLocation? Location { get; set; }
+        [JsonProperty("current")]
+        public ApiWeatherCurrent? Current { get; set; }
+        [JsonProperty("forecast")]
+        public ApiWeatherForecastForecast? Forecast { get; set; }
+    }
+}
---
+++ Weather_App/Models/ExternalApi/WeatherForecast/ApiWeatherForecastDay.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Weather_App.Models.ExternalApi.WeatherForecast
+{
+    public class ApiWeatherForecastDay
+    {
+        [JsonProperty("date")]
+        public DateTime? Date { get; set; }
+        [JsonProperty("date_epoch")]
+        public long? DateEpoch { get; set; }
+        [JsonProperty("day")]
+        public ApiWeatherForecastDayDay? Day { get; set; }
+        [JsonProperty("astro")]
+        public ApiWeatherForecastDayAstro? Astro { get; set; }
+        [JsonProperty("hour")]
+        public List<ApiWeatherForecastDayHour>? Hours { get; set; }
+    }
+}
