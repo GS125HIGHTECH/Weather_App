5a03d4a2c49c1caf48a3d10c377d2384858b8ad1
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-09 13:53:52 +0200

    ExternalApi: Added ExternalApiRootObject.

---
+++ Weather_App/Models/Shared/ExternalApiRootObject.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Weather_App.Data.Converters;
+
+namespace Weather_App.Models.Shared
+{
+    public class ExternalApiRootObject<TEntity>
+    {
+        public string Get { get; set; }
+        [JsonConverter(typeof(ToDictionaryStringStringJsonConverter))]
+        public Dictionary<string, string> Parameters { get; set; }
+        [JsonConverter(typeof(ToDictionaryStringStringJsonConverter))]
+        public Dictionary<string, string>? Errors { get; set; }
+        public int Results { get; set; }
+        public Dictionary<string, int>? Paging { get; set; }
+        public IEnumerable<TEntity> Response { get; set; }
+    }
+}
