412f53a4a9c6c9f4438ee8c91a27ce526cafe2e8
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-12 21:33:48 +0200

    ExternalApi: Added Entities consts.

---
+++ Weather_App/Consts/Entities.cs
@@ -0,0 +1,20 @@
+﻿namespace Weather_App.Consts;
+
+public static class Entities
+{
+    public const string Astro = "Astro";
+
+    public const string Condition = "Condition";
+
+    public const string Current = "Current";
+
+    public const string ForecastDay = "ForecastDay";
+
+    public const string ForecastHour = "ForecastHour";
+
+    public const string Location = "Location";
+
+    public const string WeatherCurrent = "WeatherCurrent";
+
+    public const string WeatherForecast = "WeatherForecast";
+}
