87254d982614a24c8eb9b075896e8a7e6921cf03
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-13 18:14:10 +0200

    ExternalApi: Added Weather Current and Forecast controllers.

---
+++ Weather_App/Controllers/WeatherCurrentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Weather_App.Consts;
+using Weather_App.Data.Services.WeatherCurrentService;
+
+namespace Weather_App.Controllers;
+
+[Authorize(Roles = Roles.Administrator)]
+public class WeatherCurrentController : Controller
+{
+    private readonly IWeatherCurrentService _service;
+
+    public WeatherCurrentController(IWeatherCurrentService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var weatherForecasts = await _service.GetWeatherCurrents();
+
+        return View(weatherForecasts);
+    }
+}
---
+++ Weather_App/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Weather_App.Consts;
+using Weather_App.Data.Services.WeatherForecastService;
+
+namespace Weather_App.Controllers;
+
+[Authorize(Roles = Roles.Administrator)]
+public class WeatherForecastController : Controller
+{
+    private readonly IWeatherForecastService _service;
+
+    public WeatherForecastController(IWeatherForecastService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var weatherForecasts = await _service.GetWeatherForecasts();
+
+        return View(weatherForecasts);
+    }
+}
