8131f62e39407d7050cffd12fa391c9af5b5c233
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-18 11:24:11 +0200

    WeatherView: Added task in controllers to get from service only current user weather forecasts and currents and changed previous index task to admin panel authorized task.

--- Weather_App/Controllers/WeatherCurrentController.cs
+++ Weather_App/Controllers/WeatherCurrentController.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Weather_App.Consts;
 using Weather_App.Data.Services.WeatherCurrentService;
 
 namespace Weather_App.Controllers;
 
-[Authorize(Roles = Roles.Administrator)]
 public class WeatherCurrentController : Controller
 {
     private readonly IWeatherCurrentService _service;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public WeatherCurrentController(IWeatherCurrentService service)
+    public WeatherCurrentController(IWeatherCurrentService service, 
+        UserManager<IdentityUser> userManager,
+        IHttpContextAccessor httpContextAccessor)
     {
         _service = service;
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<IActionResult> Index()
+    {
+        var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext?.User);
+
+        var weatherForecasts = await _service.GetWeatherCurrents(user.Id);
+
+        return View(weatherForecasts);
+    }
+
+    [Authorize(Roles = Roles.Administrator)]
+    public async Task<IActionResult> AdminPanel()
     {
         var weatherForecasts = await _service.GetWeatherCurrents();
 
--- Weather_App/Controllers/WeatherForecastController.cs
+++ Weather_App/Controllers/WeatherForecastController.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Weather_App.Consts;
 using Weather_App.Data.Services.WeatherForecastService;
 
 namespace Weather_App.Controllers;
 
-[Authorize(Roles = Roles.Administrator)]
 public class WeatherForecastController : Controller
 {
     private readonly IWeatherForecastService _service;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public WeatherForecastController(IWeatherForecastService service)
+    public WeatherForecastController(IWeatherForecastService service,
+        UserManager<IdentityUser> userManager,
+        IHttpContextAccessor httpContextAccessor)
     {
         _service = service;
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<IActionResult> Index()
+    {
+        var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext?.User);
+
+        var weatherForecasts = await _service.GetWeatherForecasts(user.Id);
+
+        return View(weatherForecasts);
+    }
+
+    [Authorize(Roles = Roles.Administrator)]
+    public async Task<IActionResult> AdminPanel()
     {
         var weatherForecasts = await _service.GetWeatherForecasts();
 
