0afc860ab344e92ba5d3e88fbdc0e455001e1801
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-23 17:50:19 +0200

    WeatherSearch: Added functionality to search for a location to check the weather using Google Maps API with API key stored locally in Secrets.json

---
+++ Weather_App/wwwroot/js/home/index.js
@@ -0,0 +1,89 @@
+﻿let map;
+let marker;
+let autocomplete;
+
+function initMap() {
+    const southWest = new google.maps.LatLng(-85, -180);
+    const northEast = new google.maps.LatLng(85, 180);
+    const bounds = new google.maps.LatLngBounds(southWest, northEast);
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 40, lng: 0 },
+        zoom: 3,
+        minZoom: 3,
+        restriction: {
+            latLngBounds: bounds,
+            strictBounds: false,
+        },
+        streetViewControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        styles: [
+            {
+                featureType: "poi",
+                elementType: "labels",
+                stylers: [{ visibility: "off" }],
+            },
+            {
+                featureType: "transit",
+                elementType: "labels.icon",
+                stylers: [{ visibility: "off" }],
+            },
+        ],
+    });
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+    });
+
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById("autocomplete"));
+    autocomplete.bindTo("bounds", map);
+
+    autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+
+        if (!place.geometry || !place.geometry.location) {
+            alert("No details available for the input: '" + place.name + "'");
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        marker.setPosition(place.geometry.location);
+        updateLatLng(place.geometry.location);
+    });
+
+    marker.addListener("dragend", () => {
+        updateLatLng(marker.getPosition());
+    });
+
+    map.addListener("click", (event) => {
+        marker.setPosition(event.latLng);
+        updateLatLng(event.latLng);
+    });
+}
+
+function updateLatLng(location) {
+    document.getElementById("lat").value = location.lat(); 
+    document.getElementById("lng").value = location.lng(); 
+}
+
+function submitForm() {
+    const lat = document.getElementById("lat").value; 
+    const lng = document.getElementById("lng").value; 
+    if (lat && lng) {
+        const param = `${lat},${lng}&days=3`;
+        document.getElementById("paramField").value = param;
+        document.getElementById("weatherForm").submit();
+    } else {
+        alert("Please select a location on the map.");
+    }
+}
+
+window.onload = initMap;
