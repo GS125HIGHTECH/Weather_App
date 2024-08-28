e293480848efab866a8cf6d44511e7c69f2b4b8e
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-28 12:52:27 +0200

    WeatherDto: Added DTOs for related with WeatherForecast entities.

---
+++ Weather_App/Models/Dto/WeatherForecast/ForecastHourDto.cs
@@ -0,0 +1,14 @@
+﻿using Weather_App.Models.Dto.WeatherCurrent;
+using Weather_App.Models.Entities.Shared.EntityBase;
+
+namespace Weather_App.Models.Dto.WeatherForecast;
+
+public class ForecastHourDto : EntityBase<long>
+{
+    public long ForecastDayId { get; set; }
+    public virtual ForecastDayDto? ForecastDay { get; set; }
+    public DateTime? Time { get; set; }
+    public float? TemperatureCelsius { get; set; }
+    public long ConditionId { get; set; }
+    public virtual ConditionDto? Condition { get; set; }
+}
