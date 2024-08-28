8d1e90d1ffa0948bf3cf6b1f500740450210d0cc
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-28 12:53:22 +0200

    WeatherDto: Added mapping for DTOs and entities in MappingProfiles.

--- Weather_App/Config/MappingProfiles.cs
+++ Weather_App/Config/MappingProfiles.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Weather_App.Models.Dto.WeatherCurrent;
+using Weather_App.Models.Dto.WeatherForecast;
+using Weather_App.Models.Entities;
 
 namespace Weather_App.Config;
 
@@ -6,6 +9,12 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles() 
     {
-
+        CreateMap<WeatherCurrent, WeatherCurrentDto>();
+        CreateMap<Location, LocationDto>();
+        CreateMap<Current, CurrentDto>();
+        CreateMap<Condition, ConditionDto>();
+        CreateMap<WeatherForecast, WeatherForecastDto>();
+        CreateMap<ForecastDay, ForecastDayDto>();
+        CreateMap<ForecastHour, ForecastHourDto>();
     }
 }
