7276327a2ac067be5052d0ee4cc8cf55709189cb
Grzegorz Sobczak <gs214gs@gmail.com>
2024-08-28 12:51:11 +0200

    WeatherDto: Added DTOs for related with WeatherCurrent entities.

---
+++ Weather_App/Models/Dto/WeatherCurrent/ConditionDto.cs
@@ -0,0 +1,9 @@
+﻿using Weather_App.Models.Entities.Shared.EntityBase;
+
+namespace Weather_App.Models.Dto.WeatherCurrent;
+
+public class ConditionDto : EntityBase<long>
+{
+    public string? Text { get; set; }
+    public string? Icon { get; set; }
+}
---
+++ Weather_App/Models/Dto/WeatherCurrent/LocationDto.cs
@@ -0,0 +1,10 @@
+﻿using Weather_App.Models.Entities.Shared.EntityBase;
+
+namespace Weather_App.Models.Dto.WeatherCurrent;
+
+public class LocationDto : EntityBase<long>
+{
+    public string? Name { get; set; }
+    public string? Country { get; set; }
+    public DateTime? Localtime { get; set; }
+}
