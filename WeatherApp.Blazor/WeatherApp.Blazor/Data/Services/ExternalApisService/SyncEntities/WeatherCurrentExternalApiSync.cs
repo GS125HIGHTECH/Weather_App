87c6f3fb1b1e5c3e54c3a4848dc31de6738bc17b
Grzegorz Sobczak <gs214gs@gmail.com>
2025-02-02 14:26:26 +0100

    Add services.

---
+++ WeatherApp.Blazor/WeatherApp.Blazor/Data/Services/ExternalApisService/SyncEntities/WeatherCurrentExternalApiSync.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WeatherApp.Blazor.Data.Repositories.Base;
+using WeatherApp.Blazor.Data.Repositories.Custom;
+using WeatherApp.Blazor.Models.Entities;
+using WeatherApp.Shared.Consts;
+using WeatherApp.Shared.Models.Entities;
+using WeatherApp.Shared.Models.ExternalApi.WeatherCurrent;
+
+namespace WeatherApp.Blazor.Data.Services.ExternalApisService.SyncEntities;
+
+public class WeatherCurrentExternalApiSync : ExternalApiSyncService<long, WeatherCurrentBlazor, WeatherCurrentBlazor>,
+    IExternalApiSyncService<long, WeatherCurrentBlazor, WeatherCurrentBlazor>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public WeatherCurrentExternalApiSync(
+    ICustomRepository customRepository,
+    IConfiguration configuration,
+    IHttpClientFactory httpClientFactory,
+    ILogger<ExternalApiSyncService<long, WeatherCurrentBlazor, WeatherCurrentBlazor>> logger,
+    IEntityBaseRepository<long, WeatherCurrentBlazor> entityBaseRepository,
+    UserManager<ApplicationUser> userManager,
+    IHttpContextAccessor httpContextAccessor)
+    : base(customRepository, configuration, httpClientFactory, logger, entityBaseRepository)
+    {
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<IEnumerable<WeatherCurrentBlazor>> BeginRequest(string? parameters)
+    {
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(parameters))
+            {
+                RequestInfo.Status = Statuses.Ok;
+
+                var ret = await BeginRequest<ApiWeatherRealtime>(parameters,
+                    async apiResult => await AdditionalOperation(apiResult));
+
+                await SaveRequestInformation();
+
+                return ret;
+            }
+
+            return new List<WeatherCurrentBlazor>();
+        }
+        catch (Exception ex)
+        {
+            RequestInfo.Description += "Error: " + ex.Message;
+            RequestInfo.AdditionalInformations += "Error: " + ex.InnerException?.Message;
+            RequestInfo.Status = Statuses.Error;
+            await SaveRequestInformation();
+            throw;
+        }
+    }
+
+    private async Task<IEnumerable<WeatherCurrentBlazor>> AdditionalOperation(ApiWeatherRealtime apiResult)
+    {
+        var ret = new List<WeatherCurrentBlazor>();
+        var createCounts = new Dictionary<string, int> { ["creatingWeatherCurrents"] = 0 };
+        var createdWeatherCurrents = new List<WeatherCurrentBlazor>();
+
+        if (apiResult != null && apiResult.Location != null && apiResult.Current?.Condition != null)
+        {
+            var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext?.User);
+            if (user != null)
+            {
+                var creatingWeatherCurrent = new WeatherCurrentBlazor
+                {
+                    AccountId = user.Id,
+                    Location = new Location
+                    {
+                        Name = apiResult.Location?.Name,
+                        Region = apiResult.Location?.Region,
+                        Country = apiResult.Location?.Country,
+                        Latitude = apiResult.Location?.Latitude,
+                        Longitude = apiResult.Location?.Longitude,
+                        TimeZone = apiResult.Location?.TimeZone,
+                        LocaltimeEpoch = apiResult.Location?.LocaltimeEpoch,
+                        Localtime = apiResult.Location?.Localtime
+                    },
+                    Current = new Current
+                    {
+                        LastUpdatedEpoch = apiResult.Current?.LastUpdatedEpoch,
+                        LastUpdated = apiResult.Current?.LastUpdated,
+                        TemperatureCelsius = apiResult.Current?.TemperatureCelsius,
+                        TemperatureFahrenheit = apiResult.Current?.TemperatureFahrenheit,
+                        IsDay = apiResult.Current?.IsDay,
+                        WindMPH = apiResult.Current?.WindMPH,
+                        WindKPH = apiResult.Current?.WindKPH,
+                        WindDegree = apiResult.Current?.WindDegree,
+                        WindDirection = apiResult.Current?.WindDirection,
+                        PressureMilibars = apiResult.Current?.PressureMilibars,
+                        PressureHgInches = apiResult.Current?.PressureHgInches,
+                        PrecipMillimetres = apiResult.Current?.PrecipMillimetres,
+                        PrecipInches = apiResult.Current?.PrecipInches,
+                        Humidity = apiResult.Current?.Humidity,
+                        Cloud = apiResult.Current?.Cloud,
+                        FeelsLikeTemperatureCelsius = apiResult.Current?.FeelsLikeTemperatureCelsius,
+                        FeelsLikeTemperatureFahrenheit = apiResult.Current?.FeelsLikeTemperatureFahrenheit,
+                        WindChillCelsius = apiResult.Current?.WindChillCelsius,
+                        WindChillFahrenheit = apiResult.Current?.WindChillFahrenheit,
+                        HeatIndexCelsius = apiResult.Current?.HeatIndexCelsius,
+                        HeatIndexFahrenheit = apiResult.Current?.HeatIndexFahrenheit,
+                        DewpointCelsius = apiResult.Current?.DewpointCelsius,
+                        DewpointFahrenheit = apiResult.Current?.DewpointFahrenheit,
+                        VisibilityKilometers = apiResult.Current?.VisibilityKilometers,
+                        VisibilityMiles = apiResult.Current?.VisibilityMiles,
+                        UVIndex = apiResult.Current?.UVIndex,
+                        GustMPH = apiResult.Current?.GustMPH,
+                        GustKPH = apiResult.Current?.GustKPH,
+                        Condition = apiResult.Current?.Condition != null ? new Condition
+                        {
+                            Text = apiResult.Current.Condition.Text,
+                            Icon = apiResult.Current.Condition.Icon,
+                            Code = apiResult.Current.Condition.Code
+                        } : null
+                    }
+                };
+
+                RequestInfo.AdditionalEntities = $"{Entities.Current}, {Entities.Location}";
+
+                createdWeatherCurrents.Add(creatingWeatherCurrent);
+                createCounts["creatingWeatherCurrents"]++;
+            }
+
+            await CustomRepository.CreateOrUpdateMultiAsync<long, WeatherCurrentBlazor>(createdWeatherCurrents);
+
+            RequestInfo.Description += $@"
+            Created WeatherCurrents: {createCounts["creatingWeatherCurrents"]}.";
+        }
+        else
+        {
+            RequestInfo.Status = Statuses.Error;
+            RequestInfo.Description += "Error: API response is null. ";
+        }
+
+        return ret;
+    }
+}
---
+++ WeatherApp.Blazor/WeatherApp.Blazor/Data/Services/WeatherForecastService/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using WeatherApp.Shared.Models.Dto.WeatherForecast;
+
+namespace WeatherApp.Blazor.Data.Services.WeatherForecastService;
+
+public interface IWeatherForecastService
+{
+    Task<IEnumerable<WeatherForecastBlazorDto>> GetWeatherForecasts();
+    Task<IEnumerable<WeatherForecastBlazorDto>> GetWeatherForecasts(string accountId);
+}
