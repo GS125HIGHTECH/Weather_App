198771b92d00d43997f9f5abda48976181c7ff2d
Grzegorz Sobczak <gs214gs@gmail.com>
2025-02-02 11:58:59 +0100

    Add entity models to Blazor app and Dto's to Shared.

---
+++ WeatherApp.Blazor/WeatherApp.Blazor/Models/Entities/WeatherCurrentBlazor.cs
@@ -0,0 +1,16 @@
+﻿using WeatherApp.Blazor.Data;
+using WeatherApp.Shared.Models.Entities;
+using WeatherApp.Shared.Models.Entities.Shared.EntityBase;
+
+namespace WeatherApp.Blazor.Models.Entities
+{
+    public class WeatherCurrentBlazor : EntityBase<long>
+    {
+        public string? AccountId { get; set; }
+        public virtual ApplicationUser? Account { get; set; }
+        public long LocationId { get; set; }
+        public virtual Location? Location { get; set; }
+        public long CurrentId { get; set; }
+        public virtual Current? Current { get; set; }
+    }
+}
