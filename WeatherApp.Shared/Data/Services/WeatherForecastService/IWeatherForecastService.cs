59073c1dc83147ba23e1cabf6e735fac246b455d
Grzegorz Sobczak <gs214gs@gmail.com>
2025-02-04 14:37:07 +0100

    Add interfaces to Shared project and update gitignore.

---
+++ WeatherApp.Shared/Data/Services/WeatherForecastService/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using WeatherApp.Shared.Models.Dto.WeatherForecast;
+
+namespace WeatherApp.Shared.Data.Services.WeatherForecastService;
+
+public interface IWeatherForecastService
+{
+    Task<IEnumerable<WeatherForecastBlazorDto>> GetWeatherForecasts();
+    Task<IEnumerable<WeatherForecastBlazorDto>> GetWeatherForecasts(string accountId);
+}
